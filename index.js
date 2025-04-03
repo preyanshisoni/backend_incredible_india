af7e4100f6c7f85cb3e5052b86860904aa9bbc3c
priyanshisoni <priyanshisoni7227@gmail.com>
2025-04-03 13:07:21 +0530

    add manifest.json in app.js

--- index.js
+++ index.js
@@ -26,6 +26,8 @@
 
   app.use("/admin", express.static(path.join(__dirname, "admin-panel/build")));
   app.use("/static", express.static(path.join(__dirname, "admin-panel/build/static")));
+  app.use("/admin/manifest.json", express.static(path.join(__dirname, "admin-panel/build/manifest.json")));
+
   app.get("/admin/*", (req, res) => {
   res.sendFile(path.join(__dirname, "admin-panel/build", "index.html"));
 });
@@ -35,6 +37,7 @@
 app.get("/manifest.json", (req, res) => {
   res.sendFile(path.join(__dirname, "admin-panel/build", "manifest.json"));
 });
+
 
 app.use(cors({ origin: ["http://localhost:3000","https://frontend-incredible-india.vercel.app/"] }));
   app.use("/uploads", express.static("uploads"));
